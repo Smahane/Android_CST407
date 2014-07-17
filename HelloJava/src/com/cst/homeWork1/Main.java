e4d3639ee8e2fb43fa6f0387583d4182b8628f27
Smahanita <sdouyeb@hotmail.com>
2014-07-17 16:47:43 -0700

    HomeWork # 1
    "Hello World" Java and "first android App"

---
+++ HelloJava/src/com/cst/homeWork1/Main.java
@@ -0,0 +1,15 @@
+package com.cst.homeWork1;
+
+public class Main {
+	
+	//public static anotherClass ac;
+	
+	public static void main(String[] args){
+		anotherClass an = new anotherClass();
+		
+		an.name = "Android";
+		an.print();
+	}
+	
+
+}
---
+++ HelloJava/src/com/cst/homeWork1/anotherClass.java
@@ -0,0 +1,11 @@
+package com.cst.homeWork1;
+
+public class anotherClass {
+
+	public  String name;
+	
+	public void print(){
+		System.out.println(name + "! Hello World");
+		
+	}
+}
