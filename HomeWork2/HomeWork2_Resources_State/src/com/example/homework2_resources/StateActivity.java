c2ba66aa6cc83699cd32a3e0d40bdfcb4c1fae07
Smahanita <sdouyeb@hotmail.com>
2014-07-20 11:41:52 -0700

    Submit changed to HomeWork1 and HomeWork2

---
+++ HomeWork2/HomeWork2_Resources_State/src/com/example/homework2_resources/StateActivity.java
@@ -0,0 +1,42 @@
+package com.example.homework2_resources;
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class StateActivity extends Activity{
+	private static final String State_Text = "TXT_STATE";
+	private TextView txtView = null;
+	
+	private void generateRandom(){
+		Random rd = new Random();
+		int randomNum = rd.nextInt();
+		txtView.setText(String.valueOf(randomNum));
+	}
+	
+	@Override
+	protected void onCreate (Bundle savedIstanceState){
+		super.onCreate(savedIstanceState);
+		setContentView(R.layout.activity_state);
+		
+		txtView = (TextView) findViewById(R.id.textView2);
+		generateRandom();
+		
+		if(savedIstanceState !=null){
+			String text = savedIstanceState.getString(State_Text);
+			txtView.setText(text);
+			
+		}
+	}
+	
+	@Override
+	protected void onSaveInstanceState (Bundle outState){
+		super.onSaveInstanceState(outState);
+		
+		String previousText = txtView.getText().toString();
+		outState.putString(State_Text, previousText);
+	}
+	
+}
